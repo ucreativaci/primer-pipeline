e0bb8418cf6be0c4724476863f1cc134f7ea2de5
ucreativaci <98706929+ucreativaci@users.noreply.github.com>
2022-02-21 00:21:47 -0600

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace pipeline_romell
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Potencial cambio 5");
+                    await context.Response.WriteAsync("Potencial cambio 6");
                 });
             });
         }
