a6f75cd11822fc57646a6db21f0295b16963db01
ucreativaci <98706929+ucreativaci@users.noreply.github.com>
2022-02-20 23:19:48 -0600

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace pipeline_romell
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Application Destroyed");
+                    await context.Response.WriteAsync("Application Restored");
                 });
             });
         }
