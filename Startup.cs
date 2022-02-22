e49f9ab0e8b15a51598a6b298a26bf22cc0e388d
ucreativaci <98706929+ucreativaci@users.noreply.github.com>
2022-02-21 20:36:29 -0600

    fixed typo

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace pipeline_romell
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Application from Development Branch!);
+                    await context.Response.WriteAsync("Application from Development Branch!");
                 });
             });
         }
